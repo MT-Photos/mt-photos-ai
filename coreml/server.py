fb19b010449c7fe19308fdb0c63385935cbd1927
Jiaxin Zhang <zjx219@gmail.com>
2023-11-24 10:19:50 +0800

    add log level

--- coreml/server.py
+++ coreml/server.py
@@ -160,4 +160,4 @@ def restart_program():
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, log_level="info")
